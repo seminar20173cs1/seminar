265bda382e147f867b9dfde461a77e46cdf8e258
seminar20173cs1 <seminar20173cs1@gmail.com>
2017-05-03 15:12:08 +0700

    3

--- Material Skin/Material Skin/Form2.Designer.cs
+++ Material Skin/Material Skin/Form2.Designer.cs
@@ -31,6 +31,7 @@
             this.materialFlatButton1 = new MaterialSkin.Controls.MaterialFlatButton();
             this.materialCheckBox1 = new MaterialSkin.Controls.MaterialCheckBox();
             this.materialRadioButton1 = new MaterialSkin.Controls.MaterialRadioButton();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // materialFlatButton1
@@ -81,11 +82,21 @@
             this.materialRadioButton1.Text = "materialRadioButton1";
             this.materialRadioButton1.UseVisualStyleBackColor = true;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(105, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.materialRadioButton1);
             this.Controls.Add(this.materialCheckBox1);
             this.Controls.Add(this.materialFlatButton1);
@@ -101,5 +112,6 @@
         private MaterialSkin.Controls.MaterialFlatButton materialFlatButton1;
         private MaterialSkin.Controls.MaterialCheckBox materialCheckBox1;
         private MaterialSkin.Controls.MaterialRadioButton materialRadioButton1;
+        private System.Windows.Forms.Button button1;
     }
 }
